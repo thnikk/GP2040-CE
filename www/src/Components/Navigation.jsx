a6a41101af92c03cb8ad08c05fb3f659374762a2
thnikk <thnikkshop@gmail.com>
2026-07-11 17:24:18 -0700

    Re-add configuration tab for now

--- www/src/Components/Navigation.jsx
+++ www/src/Components/Navigation.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Nav, Navbar, Button, Modal } from 'react-bootstrap';
+import { Nav, NavDropdown, Navbar, Button, Modal } from 'react-bootstrap';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import './Navigation.scss';
@@ -105,7 +105,7 @@ const Navigation = () => {
 							{t('Navigation:settings-label')}
 						</span>
 					</Nav.Link>
-					{/* <NavDropdown title={t('Navigation:config-label')}>
+					<NavDropdown title={<span style={{ display: 'inline-flex', alignItems: 'center' }}><svg viewBox="0 0 448 512" fill="currentColor" style={{ marginRight: '0.5rem' }}><path d="M288 0L160 0 128 0C110.3 0 96 14.3 96 32s14.3 32 32 32l0 132.8c0 11.8-3.3 23.5-9.5 33.5L10.3 406.2C3.6 417.2 0 429.7 0 442.6C0 480.9 31.1 512 69.4 512l309.2 0c38.3 0 69.4-31.1 69.4-69.4c0-12.8-3.6-25.4-10.3-36.4L329.5 230.4c-6.2-10.1-9.5-21.7-9.5-33.5L320 64c17.7 0 32-14.3 32-32s-14.3-32-32-32L288 0zM192 196.8L192 64l64 0 0 132.8c0 23.7 6.6 46.9 19 67.1L309.5 320l-171 0L173 263.9c12.4-20.2 19-43.4 19-67.1z"/></svg>{t('Navigation:config-label')}</span>}>
 						<NavDropdown.Item
 							as={NavLink}
 							eventKey={eventKey++}
@@ -152,7 +152,7 @@ const Navigation = () => {
 								{t('Navigation:resetSettings-label')}
 							</span>
 						</NavDropdown.Item>
-					</NavDropdown> */}
+					</NavDropdown>
 					{/* <NavDropdown title={t('Navigation:links-label')}>
 						<NavDropdown.Item href="https://gp2040-ce.info/" target="_blank">
 							{t('Navigation:docs-label')}
