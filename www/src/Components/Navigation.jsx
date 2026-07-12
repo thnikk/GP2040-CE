cecb5ae518d87569fd11cbc147d435d2210b4a1e
thnikk <thnikkshop@gmail.com>
2026-07-11 17:49:32 -0700

    Put github link in button

--- www/src/Components/Navigation.jsx
+++ www/src/Components/Navigation.jsx
@@ -166,7 +166,7 @@ const Navigation = () => {
 					</NavDropdown> */}
 				</Nav>
 				<Nav className="navbar-actions">
-					<Button variant="success" onClick={handleShow} aria-label={t('Navigation:reboot-label')}>
+					<Button variant="success" onClick={handleShow} aria-label={t('Navigation:reboot-label')} className="icon-btn">
 						<svg
 							width="20"
 							height="20"
@@ -189,16 +189,18 @@ const Navigation = () => {
 					</Button>
 					<ColorScheme />
 					<LanguageSelector />
-					<a
+					<Button
+						variant="secondary"
 						href="https://github.com/thnikk/GP2040-CE"
 						target="_blank"
 						rel="noopener noreferrer"
-						className="github-link"
+						className="icon-btn text-reset"
+						aria-label="GitHub"
 					>
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
 							<path d="M216.5 362.5c-66-8-112.5-55.5-112.5-117 0-25 9-52 24-70-6.5-16.5-5.5-51.5 2-66 20-2.5 47 8 63 22.5 19-6 39-9 63.5-9s44.5 3 62.5 8.5c15.5-14 43-24.5 63-22 7 13.5 8 48.5 1.5 65.5 16 19 24.5 44.5 24.5 70.5 0 61.5-46.5 108-113.5 116.5 17 11 28.5 35 28.5 62.5l0 52C323 491.5 335.5 500 350.5 494 441 459.5 512 369 512 257 512 115.5 397 0 255.5 0S0 115.5 0 257c0 111 70.5 203 165.5 237.5 13.5 5 26.5-4 26.5-17.5l0-40c-7 3-16 5-24 5-33 0-52.5-18-66.5-51.5-5.5-13.5-11.5-21.5-23-23-6-.5-8-3-8-6 0-6 10-10.5 20-10.5 14.5 0 27 9 40 27.5 10 14.5 20.5 21 33 21s20.5-4.5 32-16c8.5-8.5 15-16 21-21z"/>
 						</svg>
-					</a>
+					</Button>
 				</Nav>
 			</Navbar.Collapse>
 
@@ -216,6 +218,9 @@ const Navigation = () => {
 						variant="secondary"
 						onClick={() => handleReboot(BOOT_MODES.BOOTSEL)}
 					>
+						<svg viewBox="0 0 384 512" fill="currentColor" style={{ marginRight: '0.5rem', height: '1em' }}>
+							<path d="M96 0C78.3 0 64 14.3 64 32l0 96 64 0 0-96c0-17.7-14.3-32-32-32zM288 0c-17.7 0-32 14.3-32 32l0 96 64 0 0-96c0-17.7-14.3-32-32-32zM32 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l0 32c0 77.4 55 142 128 156.8l0 67.2c0 17.7 14.3 32 32 32s32-14.3 32-32l0-67.2C297 398 352 333.4 352 256l0-32c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 160z"/>
+						</svg>
 						{isRebooting !== BOOT_MODES.BOOTSEL
 							? t('Navigation:reboot-modal-button-bootsel-label')
 							: isRebooting
@@ -226,6 +231,9 @@ const Navigation = () => {
 						variant="primary"
 						onClick={() => handleReboot(BOOT_MODES.WEBCONFIG)}
 					>
+						<svg viewBox="0 0 512 512" fill="currentColor" style={{ marginRight: '0.5rem', height: '1em' }}>
+							<path d="M352 256c0 22.2-1.2 43.6-3.3 64l-185.3 0c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64l185.3 0c2.2 20.4 3.3 41.8 3.3 64zm28.8-64l123.1 0c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64l-123.1 0c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32l-116.7 0c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0l-176.6 0c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0L18.6 160C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192l123.1 0c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64L8.1 320C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6l176.6 0c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352l116.7 0zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6l116.7 0z"/>
+						</svg>
 						{isRebooting !== BOOT_MODES.WEBCONFIG
 							? t('Navigation:reboot-modal-button-web-config-label')
 							: isRebooting
@@ -236,6 +244,9 @@ const Navigation = () => {
 						variant="success"
 						onClick={() => handleReboot(BOOT_MODES.GAMEPAD)}
 					>
+						<svg viewBox="0 0 640 512" fill="currentColor" style={{ marginRight: '0.5rem', height: '1em' }}>
+							<path d="M192 64C86 64 0 150 0 256S86 448 192 448l256 0c106 0 192-86 192-192s-86-192-192-192L192 64zM496 168a40 40 0 1 1 0 80 40 40 0 1 1 0-80zM392 304a40 40 0 1 1 80 0 40 40 0 1 1 -80 0zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24l0 32 32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0 0 32c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-32-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l32 0 0-32z"/>
+						</svg>
 						{isRebooting !== BOOT_MODES.GAMEPAD
 							? t('Navigation:reboot-modal-button-controller-label')
 							: isRebooting
